aa0545b015a078c88a239854b21cce6e2c742004
LRudL <55555739+LRudL@users.noreply.github.com>
2023-03-22 21:48:42 +0000

    Maybe-kinda-working replication of accuracy numbersgit add .git add .git add .git add .git add .

--- elk/utils/results.py
+++ elk/utils/results.py
@@ -10,6 +10,40 @@ from elk.extraction.prompt_dataset import PromptDataset, PromptConfig
 from elk.extraction.extraction import ExtractionConfig, extract_hiddens, extract
 import yaml
 
+
+from elk.extraction.prompt_dataset import Prompt, PromptDataset, PromptConfig
+from elk.utils import (
+    assert_type,
+    float32_to_int16,
+    infer_label_column,
+    select_train_val_splits,
+    select_usable_devices,
+)
+from elk.extraction.generator import _GeneratorBuilder
+from dataclasses import dataclass, InitVar
+from datasets import (
+    Array3D,
+    DatasetDict,
+    Features,
+    get_dataset_config_info,
+    Sequence,
+    Split,
+    SplitDict,
+    SplitInfo,
+    Value,
+)
+from simple_parsing.helpers import field, Serializable
+from transformers import (
+    AutoConfig,
+    AutoModel,
+    AutoTokenizer,
+    BatchEncoding,
+    PreTrainedModel,
+)
+from typing import Iterable, Literal, Union, List
+import logging
+import torch
+
 RPATH = "/fsx/home-rudolf/elk-reporters"
 DEVICE = "cuda"
 
@@ -158,12 +192,295 @@ def reporter_predictions(
     for pair in contrast_pairs:
         true_str, false_str = pair
         
-        # true_tokens = tokenizer(true_str, return_tensors="pt").to(DEVICE)["input_ids"]
-        # false_tokens = tokenizer(false_str, return_tensors="pt").to(DEVICE)["input_ids"]
-        
         state_true = model_hidden_states(model, tokenizer, true_str, layer, tokens)
         state_false = model_hidden_states(model, tokenizer, false_str, layer, tokens)
         
         predicted = reporter.predict(state_true, state_false)
         predictions.append(predicted.detach()[0].item())
     return t.tensor(predictions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# copy of elk.extraction.extract_hiddens
+@torch.no_grad()
+def extract_hiddens_gen(
+    # cfg: ExtractionConfig,
+    prompt_ds,
+    model,
+    tokenizer, # MAKE SURE I THAS truncation_side="left"
+    layers = None, # layer_indices
+    token_loc = "last",
+    rank = 0,
+    world_size = 1,
+    device = DEVICE
+):
+    # prompt_ds = PromptDataset(cfg.prompts, rank, world_size, split)
+    num_variants = 1
+    if rank == 0:
+        prompt_names = prompt_ds.prompter.all_template_names
+        if num_variants >= 1:
+            print(
+                f"Using {num_variants} prompts per example: {prompt_names}"
+            )
+        elif num_variants == -1:
+            print(f"Using all prompts per example: {prompt_names}")
+        else:
+            raise ValueError(f"Invalid prompt num_variants: {num_variants}")
+
+    # AutoModel should do the right thing here in nearly all cases. We don't actually
+    # care what head the model has, since we are just extracting hidden states.
+    # model = AutoModel.from_pretrained(cfg.model, torch_dtype="auto").to(device)
+    # TODO: Maybe also make this configurable?
+    # We want to make sure the answer is never truncated
+    # tokenizer = AutoTokenizer.from_pretrained(cfg.model, truncation_side="left")
+
+    # TODO: test whether using sep_token is important, but this seems low priority
+    # sep_token = tokenizer.sep_token or "\n"
+    if not tokenizer.pad_token:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    is_enc_dec = model.config.is_encoder_decoder
+
+    def tokenize(prompt: Prompt, idx: int, **kwargs):
+        return tokenizer(
+            ([prompt.to_string(idx)]),
+            padding=True,
+            return_tensors="pt",
+            truncation=True,
+            **kwargs,
+        ).to(device)
+
+    # This function returns the flattened questions and answers. After inference we
+    # need to reshape the results.
+    def collate(prompts: list[Prompt]) -> list[list[BatchEncoding]]:
+        return [[tokenize(prompt, i) for i in range(2)] for prompt in prompts]
+
+    # If this is an encoder-decoder model we don't need to run the decoder at all.
+    # Just strip it off, making the problem
+    # equivalent to a regular encoder-only model.
+    if is_enc_dec:
+        # This isn't actually *guaranteed* by HF, but it's true for all existing models
+        if not hasattr(model, "get_encoder") or not callable(model.get_encoder):
+            raise ValueError(
+                "Encoder-decoder model doesn't have expected get_encoder() method"
+            )
+
+        model = assert_type(PreTrainedModel, model.get_encoder())
+
+    # Iterating over questions
+    layer_indices = layers or tuple(range(model.config.num_hidden_layers))
+    for prompts in prompt_ds:
+        inputs = collate(prompts)
+        hidden_dict = {
+            f"hidden_{layer_idx}": torch.empty(
+                prompt_ds.num_variants,
+                2,  # contrast pair
+                model.config.hidden_size,
+                device=device,
+                dtype=torch.float32,
+            )
+            for layer_idx in layer_indices
+        }
+        variant_ids = [prompt.template.name for prompt in prompts]
+        # decode so that we know exactly what the input was
+        text_inputs = [
+            [
+                tokenizer.decode(
+                    assert_type(torch.Tensor, variant_inputs[0].input_ids)[0]
+                ),
+                tokenizer.decode(
+                    assert_type(torch.Tensor, variant_inputs[1].input_ids)[0]
+                ),
+            ]
+            for variant_inputs in inputs
+        ]
+
+        # Iterate over variants
+        for i, variant_inputs in enumerate(inputs):
+            # Iterate over answers
+            for j, inpt in enumerate(variant_inputs):
+                outputs = model(**inpt, output_hidden_states=True)
+
+                hiddens = (
+                    outputs.get("decoder_hidden_states") or outputs["hidden_states"]
+                )
+                # First element of list is the input embeddings
+                hiddens = hiddens[1:]
+
+                # Throw out layers we don't care about
+                hiddens = [hiddens[i] for i in layer_indices]
+
+                # Current shape of each element: (batch_size, seq_len, hidden_size)
+                if token_loc == "first":
+                    hiddens = [h[..., 0, :] for h in hiddens]
+                elif token_loc == "last":
+                    hiddens = [h[..., -1, :] for h in hiddens]
+                elif token_loc == "mean":
+                    hiddens = [h.mean(dim=-2) for h in hiddens]
+                else:
+                    raise ValueError(f"Invalid token_loc: {token_loc}")
+
+                for layer_idx, hidden in zip(layer_indices, hiddens):
+                    hidden_dict[f"hidden_{layer_idx}"][i, j] = hidden
+
+        assert all([prompts[0].label == prompt.label for prompt in prompts])
+        yield dict(
+            label=prompts[0].label,
+            variant_ids=variant_ids,
+            text_inputs=text_inputs,
+            **hidden_dict,
+        )
+
+
+
+
+
+# copy of the above, not requiring a dataset
+@torch.no_grad()
+def extract_hidden_for_prompt(
+    prompts,
+    model,
+    tokenizer, # MAKE SURE IT HAS truncation_side="left"
+    layers = None, # layer_indices
+    token_loc = "last",
+    device = DEVICE
+):
+    num_variants = len(prompts)
+    if not tokenizer.pad_token:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    is_enc_dec = model.config.is_encoder_decoder
+
+    def tokenize(prompt: Prompt, idx: int, **kwargs):
+        return tokenizer(
+            ([prompt.to_string(idx)]),
+            padding=True,
+            return_tensors="pt",
+            truncation=True,
+            **kwargs,
+        ).to(device)
+
+    # This function returns the flattened questions and answers. After inference we
+    # need to reshape the results.
+    def collate(prompts: list[Prompt]) -> list[list[BatchEncoding]]:
+        return [[tokenize(prompt, i) for i in range(2)] for prompt in prompts]
+
+    # If this is an encoder-decoder model we don't need to run the decoder at all.
+    # Just strip it off, making the problem
+    # equivalent to a regular encoder-only model.
+    if is_enc_dec:
+        # This isn't actually *guaranteed* by HF, but it's true for all existing models
+        if not hasattr(model, "get_encoder") or not callable(model.get_encoder):
+            raise ValueError(
+                "Encoder-decoder model doesn't have expected get_encoder() method"
+            )
+
+        model = assert_type(PreTrainedModel, model.get_encoder())
+
+    # Iterating over questions
+    layer_indices = layers or tuple(range(model.config.num_hidden_layers))
+    
+    inputs = collate(prompts)
+    hidden_dict = {
+        f"hidden_{layer_idx}": torch.empty(
+            num_variants,
+            2,  # contrast pair
+            model.config.hidden_size,
+            device=device,
+            dtype=torch.float32,
+        )
+        for layer_idx in layer_indices
+    }
+    variant_ids = [prompt.template.name for prompt in prompts]
+    # decode so that we know exactly what the input was
+    text_inputs = [
+        [
+            tokenizer.decode(
+                assert_type(torch.Tensor, variant_inputs[0].input_ids)[0]
+            ),
+            tokenizer.decode(
+                assert_type(torch.Tensor, variant_inputs[1].input_ids)[0]
+            ),
+        ]
+        for variant_inputs in inputs
+    ]
+
+    # Iterate over variants
+    for i, variant_inputs in enumerate(inputs):
+        # Iterate over answers
+        for j, inpt in enumerate(variant_inputs):
+            outputs = model(**inpt, output_hidden_states=True)
+
+            hiddens = (
+                outputs.get("decoder_hidden_states") or outputs["hidden_states"]
+            )
+            # First element of list is the input embeddings
+            hiddens = hiddens[1:]
+
+            # Throw out layers we don't care about
+            hiddens = [hiddens[i] for i in layer_indices]
+
+            # Current shape of each element: (batch_size, seq_len, hidden_size)
+            if token_loc == "first":
+                hiddens = [h[..., 0, :] for h in hiddens]
+            elif token_loc == "last":
+                hiddens = [h[..., -1, :] for h in hiddens]
+            elif token_loc == "mean":
+                hiddens = [h.mean(dim=-2) for h in hiddens]
+            else:
+                raise ValueError(f"Invalid token_loc: {token_loc}")
+
+            for layer_idx, hidden in zip(layer_indices, hiddens):
+                hidden_dict[f"hidden_{layer_idx}"][i, j] = hidden
+
+    assert all([prompts[0].label == prompt.label for prompt in prompts])
+    return dict(
+        label=prompts[0].label,
+        variant_ids=variant_ids,
+        text_inputs=text_inputs,
+        **hidden_dict,
+    )
+
+
+def reporter_predictions2(
+    reporter, model, tokenizer, dataset_examples, layer, tokens="last"
+):
+    """Assumes the true statements come first in contrast_pairs"""
+    predictions = np.array([])
+    for prompts in dataset_examples:
+        
+        hidden = extract_hidden_for_prompt(prompts, model, tokenizer, layers=[layer], token_loc=tokens)
+        
+        hidden_state = hidden[f"hidden_{layer}"]
+        hidden_state0 = hidden_state[:,0]
+        hidden_state1 = hidden_state[:,1]
+        
+        preds = reporter.predict(hidden_state0, hidden_state1)
+        
+        correct = prompts[0].label # must be same for all prompts
+        
+        if correct == 1:
+            preds = preds * -1
+        
+        predictions = np.concatenate([predictions, preds.detach().cpu().numpy()])
+    return predictions
